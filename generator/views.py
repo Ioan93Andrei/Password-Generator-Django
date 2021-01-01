9bbabe8a14ef984865b5ad75592791ebe102e438
Ioan-Andrei Ojica <ivan.andrei93@mail.ru>
2021-01-01 12:55:32 +0100

    the standard beginning for a Django project

---
+++ generator/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+# Create your views here.
+def home(request):
+  return render(request, "generator/home.html", {"password": "123456abc"})
+
+def password(request):
+  return render(request, "generator/password.html")
+  
